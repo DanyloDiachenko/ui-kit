446660f7b2176cc6c3a7703fdbdf12bbb9046386
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-24 16:14:42 +0200

    htag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./components/ui/Button";
+import { Htag } from "./components/ui/Htag";
 
 const App = () => {
     return (
@@ -8,7 +9,12 @@ const App = () => {
                 <Button appearance="purple">Continue</Button>
                 <Button appearance="red">Discard</Button>
             </div>
-            <div></div>
+            <div>
+                <Htag tag="h1">Hello, h1!</Htag>
+                <Htag tag="h2">Hello, h2!</Htag>
+                <Htag tag="h3">Hello, h3!</Htag>
+                <Htag tag="h4">Hello, h4!</Htag>
+            </div>
         </>
     );
 };
---
+++ src/components/ui/Htag/index.tsx
@@ -0,0 +1,38 @@
+import { HtagProps } from "./htag.props";
+import styles from "./styles.module.scss";
+
+export const Htag = ({ tag, children, className }: HtagProps) => {
+    switch (tag) {
+        case "h1": {
+            return (
+                <h1 className={`${styles.h1} ${className ? className : ""}`}>
+                    {children}
+                </h1>
+            );
+        }
+        case "h2": {
+            return (
+                <h2 className={`${styles.h2} ${className ? className : ""}`}>
+                    {children}
+                </h2>
+            );
+        }
+        case "h3": {
+            return (
+                <h3 className={`${styles.h3} ${className ? className : ""}`}>
+                    {children}
+                </h3>
+            );
+        }
+        case "h4": {
+            return (
+                <h4 className={`${styles.h4} ${className ? className : ""}`}>
+                    {children}
+                </h4>
+            );
+        }
+        default: {
+            return <>{children}</>;
+        }
+    }
+};
