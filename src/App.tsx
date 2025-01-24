bb60459762ba1b491c92eb9247a23a73c798d1c6
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-24 15:58:09 +0200

    btns

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.scss";
+import { Button } from "./components/ui/Button";
 
 const App = () => {
-    return <></>;
+    return (
+        <>
+            <div>
+                <Button appearance="gray">Read Smth</Button>
+                <Button appearance="purple">Continue</Button>
+                <Button appearance="red">Discard</Button>
+            </div>
+            <div></div>
+        </>
+    );
 };
 
 export default App;
